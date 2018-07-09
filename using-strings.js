381493365beb892a880782c894f13ce485504a5b
Bybreen Samuels <bybreensamuels@googlemail.com>
2018-07-09 07:54:18 +0100

    working-with-strings
    I stored the phrase home sweet home in a variable called saying.
    Then I showed how many characters are in the string using the length property
    of the string object. The result is stored in the variable called msg.

---
+++ using-strings.js
@@ -0,0 +1,5 @@
+var saying = 'Home sweet home';
+var msg = '<h2>length</h2><p>' + saying.length + '</p>;
+msg += '<h2>uppercase</h2>uppercase</h2><p>' + saying.toUpperCase()+'</p>';
+msg +='<h2>lowercase</h2><p>'+saying,toLowerCase()+'</p>';
+msg += '<h2>characterindex: 12<h2><p>' + saying.charAt(12) + '</>';
