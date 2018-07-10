61f33f6835ad1e4a96b0bc356aea28213592b80c
Bybreen Samuels <bybreensamuels@googlemail.com>
2018-07-10 17:17:00 +0100

    variables-dot-strings-objectmethods

---
+++ more-about-strings.js
@@ -0,0 +1,4 @@
+msg += '<h2>first ee</h2><p>' + saying.indexOf('ee') + '</p>';
+msg += '<h2> last ee</h2><p>' + saying.last indexOf('e) + '</p>';
+msg += '<h2>character index : 8-14 </h2><p>' + saying.substring (8, 14) + '</p>';
+msg += '<h2>replace</h2><p>' + saying.replace me, wj + '</p>';
