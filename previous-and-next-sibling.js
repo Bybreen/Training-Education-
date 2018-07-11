86994fcc87d6c2276876c275ba9f9e716858922a
Bybreen Samuels <bybreensamuels@googlemail.com>
2018-07-11 08:18:05 +0100

    node-siblings-
    These methods are used to cover the dom. The previous and next siblings move through the parent node list.

---
+++ previous-and-next-sibling.js
@@ -0,0 +1,5 @@
+var starItem = document.getElementById('two');
+var prevItem =starItem.previousSibling;
+var nextItem = starItem.nextSibling;
+prevItem.className ='complete':
+nextItem.className ='cool';
