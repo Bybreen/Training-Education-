3c9fa56d41228c95ae82a9e4cd0f8ec2d6c87275
Bybreen Samuels <bybreensamuels@googlemail.com>
2018-07-12 14:23:59 +0100

    use-these-properties-for-an-unordered-list

---
+++ first-and-last-child.js
@@ -0,0 +1,5 @@
+var startItem = document.getElementsByTagName('ul)[0];
+var firstItem = startItemfirstchild;
+var lastItem = startItem.lastchild;
+firstItem.setAttribute('class';complete')
+lastItem.setAttribute('class;cool');
