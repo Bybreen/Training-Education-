4d286dc1bb872c9159f2b8e63034a08bb562f4c1
Bybreen Samuels <bybreensamuels@googlemail.com>
2018-08-24 09:35:19 +0100

    mutation-events
    They occur when elements are added to or removed from the DOM.

---
+++ using-mutation-events.js
@@ -0,0 +1,10 @@
+var elList.addLink, newEl, newText, counter, listItems;
+elList = document.getElementById('list');
+addLink = document.gedChild(newTexttElementById('counter');
+function addItem(e) {
+e.preventDefault();
+newEl = document.createElement('li');
+newText = document.createTextNode('New list item');
+newEl.appendChild(newText);
+elList.appendChild(newEl);
+}
