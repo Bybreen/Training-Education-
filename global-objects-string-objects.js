339189d57e29387633c335094c6219f357ebaf3a
Bybreen Samuels <bybreensamuels@googlemail.com>
2018-08-29 07:57:59 +0100

    string-objects
    They allow you to work with a range of different characters.

---
+++ global-objects-string-objects.js
@@ -0,0 +1,10 @@
+Property e.g.
+length saying.length Home Sweet Home
+                     Result 16
+Description it returns the number of characters in the string
+
+Method e.g.
+to UpperCase() saying.toUpperCase();
+               Home sweet home
+               Result'HOME SWEET HOME'
+Description it changes string to upper case
