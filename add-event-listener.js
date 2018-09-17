56b7a5ea7ed679a354b0072d27ef5d450d59642f
Bybreen Samuels <bybreensamuels@googlemail.com>
2018-09-17 12:41:24 +0100

    add-event-listener
    Event listeners are placed inside conditional statements so the attach event() can be used for visitors who have IE8 or less.

---
+++ add-event-listener.js
@@ -0,0 +1,10 @@
+if(document.addEventListener){
+document.addEventListener('click', function(e){
+recorderControls(e);
+}.false);
+username.addEventListener('input', writeLabel.false);
+} else {
+document.attach.Event('onclick',
+function(e){
+recorderControls(e);
+unsername.attachEvent('onkeyup', writeLabel, false);
