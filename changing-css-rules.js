87182bd4b70fb2e03fbfa03719ced97db9e2ac09
Bybreen Samuels <bybreensamuels@googlemail.com>
2018-10-02 11:27:37 +0100

    setting-and-changing-css-rules
    The .css() can be used to select and update the css properties of elements. The script checks what the background colour of the first list item is when the page loads and then writes it after the list.
    Next, it updates several css properties in all list items using the same .css() method with object literal notation.

---
+++ changing-css-rules.js
@@ -0,0 +1,11 @@
+$(function() {
+var backgroundColor=$('li').css(background-color');
+$('ul').append('<p>Color was: '+ backgroundColor+ '</p>');
+$('li').css ({
+'background-color': '#5a 996'.
+'border': '1px solid #fff',
+'color': '#000;
+'font-family'; 'Georgia',
+'padding-left: '+ = 75'
+});
+});
