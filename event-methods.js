2e13921192158f055c2adfefc3aae8caf3f7c680
Bybreen Samuels <bybreensamuels@googlemail.com>
2018-10-04 08:22:43 +0100

    events-relating-to-a-list-item
    When you move the mouse over a list item the content of its id attribute is written into the list item.
    The mouse out event also removes the extra information from the page to prevent the added content building up.

---
+++ event-methods.js
@@ -0,0 +1,7 @@
+$~(function() {
+var ids = ";
+var $listItems = $('li');
+$listItems.on('mouseover click', function() {
+ids = this.id;
+$listItemns.children('span'.remove();
+$(this).append('<span class = "priority"> + ids + '</span>');
