7a4eb12de46ae6f31ff51e5cf9f50bd3afcc4c7b
Bybreen Samuels <bybreensamuels@googlemail.com>
2018-10-09 07:21:04 +0100

    event-handlers-for-delegating-events
    The event handlers run when the user clicks or mouseover items on the list, except for the last item.
    It writes out the content of the element the user interacted with. A status message (using the data property)
    and the event type. The info passed in the data property here uses object literal notation (so it can handle
    multiple properties.

---
+++ delegating-events.js
@@ -0,0 +1,15 @@
+$(function()
+
+var listItem, itemStatus, eventType;
+$)'ul').on(
+click mouseover',
+':not(#four)',
+(status: 'important');
+function(e) {
+listItem = '+ e.targetContent + '<br/>';
+itemStatus = 'Status: '+ e.data.status' + '<br/>;
+eventType = 'Event: ' + e.type;
+$('#notes').html(listItem + itemStatus + eventType);
+)
+);
+});
