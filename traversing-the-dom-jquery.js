b4236f24adea9210dc3fa207ed4f96750a1b7291
Bybreen Samuels <bybreensamuels@googlemail.com>
2018-11-15 08:33:28 +0000

    jquery-traversing-the-dom
    When web pages load an unordered list maybe hidden within the 2nd heading. However, a link maybe included and if the user clicks on it, the list will be revealed.

---
+++ traversing-the-dom-jquery.js
@@ -0,0 +1,12 @@
+$(function() {
+var $h2 = $('h2');
+$('ul').hide();
+$h2.append('<a>show</a>');
+$h2.on('click; function(){
+$h2.next()
+.fadeIn (500)
+.children(.hot')
+.addClass('complete');
+$h2.find('a').fadeOut();
+});
+});
